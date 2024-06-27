61bd10f72af2b0b02e5b425eef3f4580f3587106
Hendrik <fanriohendrik@gmail.com>
2024-06-27 14:57:30 +0700

    update

---
+++ src/main/java/com/example/demo/DemoForm.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class DemoForm {
+    private String iso8583;
+
+    public DemoForm() {
+    }
+
+    public DemoForm(String iso8583) {
+        super();
+        this.iso8583 = iso8583;
+    }
+    
+    public String getIso8583() {
+        return iso8583;
+    }
+
+    public void setUsername(String iso8583) {
+        this.iso8583 = iso8583;
+    }
+
+   
+} 
